d7a444c6d4d383be484bbaf52867b5df7b9bf90c
Chris Morrell <inxilpro@users.noreply.github.com>
2022-10-02 13:36:17 -0400

    Remove prepare to wait

--- src/Browser/Concerns/ExecutesWaitCommands.php
+++ src/Browser/Concerns/ExecutesWaitCommands.php
@@ -4,7 +4,6 @@ namespace Glhd\Dawn\Browser\Concerns;
 
 use Closure;
 use Facebook\WebDriver\WebDriverExpectedCondition;
-use Glhd\Dawn\Browser\Commands\Wait\PrepareToWaitForReload;
 use Glhd\Dawn\Browser\Commands\Wait\WaitUsing;
 
 /**
@@ -14,12 +13,6 @@ use Glhd\Dawn\Browser\Commands\Wait\WaitUsing;
  */
 trait ExecutesWaitCommands
 {
-	/** @return $this|mixed */
-	public function prepareToWaitForReload(): mixed
-	{
-		return $this->command(new PrepareToWaitForReload());
-	}
-	
 	public function waitUsing(?int $seconds, int $interval, Closure|WebDriverExpectedCondition $wait, ?string $message = null): static
 	{
 		return $this->command(new WaitUsing($seconds, $interval, $wait, $message));
